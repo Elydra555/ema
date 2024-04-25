56a1b89f35b5bca061df9fac9fcefa93467951ee
Kovács Ferenc Richárd <kliberregobenedek@ktch.hu>
2024-04-25 08:36:00 +0200

    bdgsf

--- web/src/app.js
+++ web/src/app.js
@@ -1,29 +1,29 @@
 const doc = {
     empBody: document.querySelector("#empBody"),
     addButton: document.querySelector("#addButton"),
-    name: document.querySelector("#name"),
-    city: document.querySelector("#city"),
-    salary: document.querySelector("#salary")
+    nameInput: document.querySelector("#name"),
+    cityInput: document.querySelector("#city"),
+    salaryInput: document.querySelector("#salary")
 }
 
 const state = {
     url: 'http://localhost:8000/employees',
-    name: "névtelen",
-    city: "ismeretlen",
+    name: 'névtelen',
+    city: 'ismeretlen',
     salary: 300
 }
 
 
 doc.addButton.addEventListener('click', () => {
-    console.log('működik')
+    console.log('műkődik')
     getDataFromForm()
     createEmployee()
 })
 
-function getDataFromForm(){
-    state.name = doc.name.value
-    state.city = doc.city.value
-    state.salary = doc.salary.value
+function getDataFromForm() {
+    state.name = doc.nameInput.value
+    state.city = doc.cityInput.value
+    state.salary = doc.salaryInput.value
 }
 
 function createEmployee() {
